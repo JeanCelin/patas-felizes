b75439a9c584750a7ea63ae8a515537130cbbbba
Jean Celin <jean_estudos@hotmail.com>
2025-04-03 13:46:35 -0300

    created main-mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${unkempt.variable}`}>
-        <Navbar />
+        {/* <Navbar /> */}
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,38 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import mainDog from "../../public/caramelo-cut.png";
+import logo from "../../public/logo2.png";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-     
+    <div className={styles.home}>
+      <header className={styles.home__header}>
+        <Image src={logo} width={60} height={60} alt="logo" />
+        <h1>Patas Carentes</h1>
+      </header>
+      <main className={styles.home__main}>
+        <h2 className={styles.home__main__title}>Adote um Amigo</h2>
+        <p className={styles.home__main__text}>
+          Faça a diferença: adote um animal carente ou faça uma doação
+        </p>
+        <Link href="/adocao">
+          <button className={styles.home__main__button}>
+            Quero ajudar!
+          </button>
+        </Link>
+
+        <div style={{ position: "relative", width: "400px", height: "400px" }}>
+          <Image
+            src={mainDog}
+            alt="Cachorro Caramelo"
+            fill
+            className={styles.home__main__dog}
+            style={{ objectFit: "contain" }}
+          />
+        </div>
+      </main>
     </div>
   );
 }
+// sizes="(max-width: 400px) 70vw, 30vw"
