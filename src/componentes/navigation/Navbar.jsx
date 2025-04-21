a5aba5c7d27e1d75cfc68b57e634f134f99e727d
Jean Celin <jean_estudos@hotmail.com>
2025-04-21 15:17:03 -0300

    ajustes na responsividade da navbar

--- src/componentes/navigation/Navbar.jsx
+++ src/componentes/navigation/Navbar.jsx
@@ -10,36 +10,38 @@ import ButtonBurger from "../ui/button/ButtonBurger";
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div className={styles.navbar__logo}>
-        <Image src={icon} alt="Logo" width={25} height={25} priority />
-        <p className={styles.navbar__title}>Patas Carentes</p>
-      </div>
-      <div className={styles.navbar__links__list}>
-        <ul className={styles.navbar__list}>
-        <li className={styles.navbar__link}>
-            <Link href="#">Inicio</Link>
-          </li>
-          <li className={styles.navbar__link}>
-            <Link href="#">Sobre</Link>
-          </li>
-          <li className={styles.navbar__link}>
-            <Link href="#">Adoções</Link>
-          </li>
-          <li className={styles.navbar__link}>
-            <Link href="#">Doar</Link>
-          </li>
-          <li className={styles.navbar__link}>
-            <Link href="#">Voluntariado</Link>
-          </li>
-          <li className={styles.navbar__link}>
-            <Link href="#">Contato</Link>
-          </li>
-        </ul>
-      </div>
+      <div className={styles.navbar__wrapper}>
+        <div className={styles.navbar__logo}>
+          <Image src={icon} alt="Logo" width={25} height={25} priority />
+          <p className={styles.navbar__title}>Patas Carentes</p>
+        </div>
+        <div className={styles.navbar__links__list}>
+          <ul className={styles.navbar__list}>
+            <li className={styles.navbar__link}>
+              <Link href="#">Inicio</Link>
+            </li>
+            <li className={styles.navbar__link}>
+              <Link href="#">Sobre</Link>
+            </li>
+            <li className={styles.navbar__link}>
+              <Link href="#">Adoções</Link>
+            </li>
+            <li className={styles.navbar__link}>
+              <Link href="#">Doar</Link>
+            </li>
+            <li className={styles.navbar__link}>
+              <Link href="#">Voluntariado</Link>
+            </li>
+            <li className={styles.navbar__link}>
+              <Link href="#">Contato</Link>
+            </li>
+          </ul>
+        </div>
 
-      <ButtonPrimary children={"Doar Agora"} maxWidth="fit-content" />
-      <div className={styles.navbar__btn__burger}>
-        <ButtonBurger />
+        <ButtonPrimary children={"Doar Agora"} maxWidth="fit-content" />
+        <div className={styles.navbar__btn__burger}>
+          <ButtonBurger />
+        </div>
       </div>
     </nav>
   );
