87c306987dbbf4b6f2a4c54d098e53f07bc08d6d
Jean Celin <jean_estudos@hotmail.com>
2025-04-21 15:06:26 -0300

    feito hero section responsivo

--- src/componentes/sections/hero/HeroSection.jsx
+++ src/componentes/sections/hero/HeroSection.jsx
@@ -11,28 +11,35 @@ import description from "@/styles/Description.module.css";
 export default function HeroSection() {
   return (
     <section className={styles.hero}>
-      <div className={styles.hero__text}>
-        <div className={tag.tag}>
-          <h1>Resgate e Cuidado Animal</h1>
+      <div className={styles.hero__wrapper}>
+        <div>
+          <div className={styles.hero__text}>
+            <div className={tag.tag}>
+              <h1>Resgate e Cuidado Animal</h1>
+            </div>
+            <h2 className={`${description.title} ${styles.hero__title__size}`}>
+              Ajude a Dar Uma Segunda Chance
+            </h2>
+            <p className={`${description.text} ${styles.hero__text__size}`}>
+              Patas Carentes é dedicada ao resgate, reabilitação e adoção de
+              animais abandonados. Todo animal merece amor, cuidado e um lar
+              para sempre.
+            </p>
+          </div>
+          <div className={styles.hero__buttons}>
+            <ButtonPrimary children={"Adote um Pet"} />
+            <ButtonSecondary children={"Faça uma Doação"} />
+          </div>
         </div>
-        <h2 className={description.title}>Ajude a Dar Uma Segunda Chance</h2>
-        <p className={description.text}>
-          Patas Carentes é dedicada ao resgate, reabilitação e adoção de animais
-          abandonados. Todo animal merece amor, cuidado e um lar para sempre.
-        </p>
-      </div>
-      <div className={styles.hero__buttons}>
-        <ButtonPrimary children={"Adote um Pet"} />
-        <ButtonSecondary children={"Faça uma Doação"} />
-      </div>
-      <div className={styles.hero__image}>
-        <Image
-          src={placeholder}
-          alt="Animal resgatado"
-          fill
-          className={styles.image}
-          loading="lazy"
-        />
+        <div className={styles.hero__image}>
+          <Image
+            src={placeholder}
+            alt="Animal resgatado"
+            fill
+            className={styles.image}
+            loading="lazy"
+          />
+        </div>
       </div>
     </section>
   );
