29aaa6a9b347ad22e13473db8f13b02aba995d53
Jean Celin <jean_estudos@hotmail.com>
2025-04-30 14:23:19 -0300

    ajustado componente OtherDonation para telas grandes

--- src/componentes/comp-pages/geral/other-donations/OtherDonations.jsx
+++ src/componentes/comp-pages/geral/other-donations/OtherDonations.jsx
@@ -4,6 +4,8 @@ import styles from "./OtherDonations.module.css";
 export default function OtherDonations() {
   return (
     <section className={styles.OtherDonations}>
+      <div className={styles.OtherDonations__wrapper}>
+
       <header className={styles.OtherDonations__header}>
         <h2 className={styles.OtherDonations__header__title}>
           Outras Formas de Doar
@@ -18,18 +20,19 @@ export default function OtherDonations() {
           title="Doação Mensal"
           text="Torne-se um doador recorrente e ajude-nos a planejar melhor nossos recursos. Doações mensais, mesmo pequenas, fazem uma grande diferença."
           btnText="Seja um doador Mensal"
-        />
+          />
         <TextCard
           title="Doação de Suprimentos"
           text="Aceitamos doações de ração, medicamentos, cobertores, brinquedos e outros itens essenciais para o cuidado dos animais."
           btnText="Ver Lista de Necessidades"
-        />
+          />
         <TextCard
           title="Apadrinhe um Animal"
           text="Você pode apadrinhar um animal específico, contribuindo mensalmente para seus cuidados até que ele seja adotado."
           btnText="Animais para Apadrinhamento"
-        />
+          />
       </section>
+          </div>
     </section>
   );
 }
