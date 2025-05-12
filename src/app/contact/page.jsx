77a9f65c371c68b2f9d05a9d3858dc3fba3ae108
Jean Celin <jean_estudos@hotmail.com>
2025-05-12 13:22:49 -0300

    add componente contact form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,13 @@
 import ContactHero from "@/componentes/comp-pages/contact/hero/ContactHero";
+import ContactForm from "@/componentes/comp-pages/contact/contact-form/ContactForm";
 
 export default function contact() {
-  return <>
-    <ContactHero />
-  </>;
+  return (
+    <>
+      <ContactHero />
+      <div>
+        <ContactForm />
+      </div>
+    </>
+  );
 }
